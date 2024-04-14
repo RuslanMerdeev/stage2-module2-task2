a1d470a1c9d6562ad6d89a031d7f61b40e97579e
ruslanmerdeev <r.merdeev@haulmont.com>
2024-04-14 12:27:07 +0300

    [master] Filled up the servlets

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,42 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import com.example.Users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Object user = req.getAttribute("user");
+        if (user == null) {
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/user/hello.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        List<String> users = Users.getInstance().getUsers();
+        boolean correctLogin = login != null && users.contains(login);
+        boolean correctPassword = password != null && !password.isEmpty();
+
+        if (correctLogin && correctPassword) {
+            req.setAttribute("user", login);
+            req.getRequestDispatcher("/user/hello.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
 }
