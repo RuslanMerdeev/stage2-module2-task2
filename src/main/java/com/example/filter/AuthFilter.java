1c932bd10d57e428fa0a84fe3f6c87175443b4d0
ruslanmerdeev <r.merdeev@haulmont.com>
2024-04-14 13:39:22 +0300

    [master] Filled up the filter and listener

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,26 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {
+        "/user/*"
+    }
+)
+public class AuthFilter implements Filter {
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        Object user = request.getAttribute("user");
+        if (user == null) {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
 }
